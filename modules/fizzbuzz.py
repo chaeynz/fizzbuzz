2a26d4f06591f66c45c16930a92004b19a614c85
chaeynz <chaeynz4msg@gmail.com>
2025-07-16 20:18:14 +0200

    Now capitalizes Fizzbuzz cache and improved type hints

--- modules/fizzbuzz.py
+++ modules/fizzbuzz.py
@@ -3,18 +3,19 @@ import os
 DEBUG =  os.environ.get("DEBUG")
 
 def fizz_buzz(fizzbuzzes, sample_range_start, sample_range_end) -> list[str]:
-    result: list = []
+    result: list[str] = []
 
     for i in range(
         sample_range_start,
         sample_range_end + 1
     ):
-        cache = ""
+        cache: str = ""
         for item in fizzbuzzes:
             if i % item["value"] == 0:
                 cache += item["name"]
 
         if cache:
+            cache = cache.capitalize()
             result.append(cache)
             if DEBUG:
                 print(cache)
