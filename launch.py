eb47b52d80e86977eb4d2b2cbac9d5625dc344b6
chaeynz <chaeynz4msg@gmail.com>
2025-07-16 20:13:11 +0200

    Implement reverse

--- launch.py
+++ launch.py
@@ -9,6 +9,12 @@ if __name__ == '__main__':
         config = json.load(f)
         fizzbuzzes = config["fizzbuzzes"]
         test_range = config["test_range"]
+        reverse = config["reverse"]
+
+    if reverse:
+        fizzbuzzes.reverse()
+
+    print(fizzbuzzes)
 
     result = fizz_buzz(fizzbuzzes, test_range["start"], test_range["end"])
     print(result)
